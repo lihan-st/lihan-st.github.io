785fd413601cce687af32b0b5d19ee26a6643517
li han <hanli8005466@gmail.com>
2025-12-02 22:15:50 -0500

    create gh-pages branch

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Users/amyhan/Desktop/gitproject/personalwebsite/my-website/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/Users/amyhan/Desktop/gitproject/personalwebsite/my-website/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/amyhan/Desktop/gitproject/personalwebsite/my-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/amyhan/Desktop/gitproject/personalwebsite/my-website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/amyhan/Desktop/gitproject/personalwebsite/my-website/src/css/custom.css"),
+];
